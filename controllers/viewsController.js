9dae47ccb111a29a2852898ebbf908f0f65d20bf
Piotr <piotrektymula1@gmail.com>
2023-05-12 13:22:32 +0200

    small changes

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   connectionLimit: 10,
   host: process.env.HOST,
   user: process.env.USER,
-  password: null,
+  password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
 
@@ -50,6 +50,26 @@ exports.getSubjects = (req, res) => {
     });
   });
 };
+exports.loadSubject = (req,res) => {
+  const slug = req.params.slug;
+  console.log(slug);
+  db.getConnection(async (err, connection) => {
+    if (err) throw err;
+    const sqlSearch = "Select * from subjects where slug = ?";
+    const search_query = mysql.format(sqlSearch, [slug]);
+    connection.query(search_query, async (err, result) => {
+      connection.release();
+      if (result.length == 0) {
+        res.status(404).json({ message: "There is no such subject" });
+      } else {
+        res.status(200).render("subject", {
+          title: 'subject',
+          subject: result
+        });
+      }
+    });
+  });
+}
 
 exports.getCourses = (req, res) => {
   db.getConnection((err, connection) => {
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12255,11 +12255,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62688" + '/');
-=======
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "60432" + '/');
->>>>>>> piotr_przedmioty
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
